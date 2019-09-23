82c4c51e784ccd31e7a02b1d4638e793d74770c0
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2019-09-23 13:06:25 +0530

    Update issues.pipe.ts

--- issueTrackerApp/src/app/issuesComponent/issues.pipe.ts
+++ issueTrackerApp/src/app/issuesComponent/issues.pipe.ts
@@ -3,10 +3,11 @@ import { PipeTransform, Pipe } from "@angular/core";
 @Pipe({name : 'issueFilter'})
 export class IssueFilter implements PipeTransform{
 
-    transform(value , args){                                   /* value is corresponding data in issue array , args is passed after : in HTML */
+    transform(value , args){                                   /* value is issue array , args is passed after : in HTML */
         
         let searchfilter = args ? args.toLowerCase() : null;   /* check if anything passed as arg or not */
 
+                /* array.filter(func) creates a new array with all elements that pass the test implemented by the provided function. */
         return searchfilter ? value.filter(issue => issue.desc.toLowerCase().indexOf(searchfilter)!=-1):value;
     }
 
