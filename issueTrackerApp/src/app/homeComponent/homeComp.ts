455b26fd7784089e331b3fc68849cf2a565d77e0
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2022-03-05 09:19:28 +0530

    Delete issueTrackerApp directory

--- issueTrackerApp/src/app/homeComponent/homeComp.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    templateUrl : './homeComp.html',
-    })
-
-    export class HomeComponent{
-        homemsg = "This is an IssueTracker application and you are right here at HOME";
-    }
--- issueTrackerApp/src/app/issuesComponent/issueinterface.ts
+++
@@ -1,9 +0,0 @@
-export interface Issue {
-    id: number;
-    desc: string;
-    severity: string;
-    status: string;
-    datecr: string;
-    daters: string;
-    selected:boolean;
-}
